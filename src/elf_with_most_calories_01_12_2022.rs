3823d3b6a48b91a8c56b56fa11695cdb1f76dae4
stagefright5 <sankarshanaj@gmail.com>
2022-12-10 19:34:14 +0530

    Day 1 - Part 2

--- src/elf_with_most_calories_01_12_2022.rs
+++ src/elf_with_most_calories_01_12_2022.rs
@@ -1,14 +1,15 @@
-// use std::env;
 use std::fs;
 
 pub fn run() {
     const FILE_PATH: &str = "inputs/elf_with_most_calories_01_12_2022.txt";
     match fs::read_to_string(FILE_PATH) {
         Ok(content) => {
-            let (max, elf_num) = compute_max_calories(content);
-            // let elf_num = idx + 1;
+            let (max, elf_num) = compute_max_calories(content.clone());
             println!("------------------------------------");
             println!("elf {elf_num} has max cals of {max}cals");
+            let sum_of_max_3 = get_sum_of_max_3_calories(content);
+            println!("------------------------------------");
+            println!("Sum of max 3 cals is {sum_of_max_3}");
         }
         Err(e) => {
             println!("{e}");
@@ -25,7 +26,7 @@ fn compute_max_calories(content: String) -> (u32, usize) {
         if !line.is_empty() {
             total_calories += line.parse::<u32>().unwrap();
         } else {
-            println!("elf {elf_num} has {total_calories}cals.");
+            // println!("elf {elf_num} has {total_calories}cals.");
             if max < total_calories {
                 max = total_calories;
                 max_elf_num = elf_num;
@@ -35,4 +36,23 @@ fn compute_max_calories(content: String) -> (u32, usize) {
         }
     }
     return (max, max_elf_num);
+}
+
+fn get_sum_of_max_3_calories(content: String) -> u32 {
+    let mut total_calories: Vec<u32> = Vec::new();
+    let mut elf_cals = 0;
+    for line in content.lines() {
+        if !line.is_empty() {
+            elf_cals += line.parse::<u32>().unwrap();
+        } else {
+            total_calories.push(elf_cals);
+            elf_cals = 0;
+        }
+    }
+    total_calories.sort_unstable_by(|a, b| (*b).cmp(a));
+    return total_calories[..=2]
+        .to_vec()
+        .into_iter()
+        .reduce(|acc, item| acc + item)
+        .unwrap();
 }
